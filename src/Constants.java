5e3b98d0729f6205252693d4648079afc612bb0c
pigmanrocker <pigmanrocker@gmail.com>
2017-03-22 13:08:39 +0100

    basic Setup

---
+++ src/Constants.java
@@ -0,0 +1,5 @@
+/**
+ * Created by pigmanrocker on 22-3-17.
+ */
+public class Constants {
+}
---
+++ src/Gui.java
@@ -0,0 +1,9 @@
+/**
+ * Created by pigmanrocker on 22-3-17.
+ */
+public class Gui {
+
+    public Gui(){
+
+    }
+}
