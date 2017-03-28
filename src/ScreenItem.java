049f8df0115a87a1553f22b6352de9b793203ed9
pigmanrocker <pigmanrocker@gmail.com>
2017-03-28 12:53:59 +0200

    added the gamescreen code

---
+++ src/ScreenItem.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+/**
+ * Created by pigmanrocker on 28-3-17.
+ */
+public interface ScreenItem {
+    Image getImage();
+    int getXCoord();
+    int getYCoord();
+    int getHeight();
+    int getWidth();
+}
